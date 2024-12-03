7c572a70db0f6f5461c42484bdd593d2bc9a6729
Melisa Akkuş <melisa.akkus01@gmail.com>
2024-12-03 22:50:04 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        Context context = new Context();    
+        public ActionResult Index()
+        {
+            var values = context.Blogs.ToList();
+            return View(values);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+            var blogs = context.Blogs.Where(x=>x.Id == id).ToList();
+            return View(blogs);
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
